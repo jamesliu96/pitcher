13151097bce05b2d3c4860ac9a50f0223e57c40a
James Liu <jamesliu96@users.noreply.github.com>
2021-12-10 19:47:47 +0800

    publish

---
+++ build/main.js
@@ -0,0 +1,119 @@
+import { autoCorrelate, getDataFromFrequency } from './pitcher.js';
+window.addEventListener('load', () => {
+    const $volumeMeter = document.getElementById('volume_meter');
+    const $frequency = document.getElementById('frequency');
+    const $nodeFrequency = document.getElementById('node_frequency');
+    const $note = document.getElementById('note');
+    const $octave = document.getElementById('octave');
+    const $deviation = document.getElementById('deviation');
+    const WIDTH = 400;
+    const HEIGHT = 100;
+    const $graphF = document.getElementById('graph_f');
+    const $graphT = document.getElementById('graph_t');
+    $graphF.width = $graphT.width = WIDTH * devicePixelRatio;
+    $graphF.height = $graphT.height = HEIGHT * devicePixelRatio;
+    $graphF.style.width = $graphT.style.width = `${WIDTH}px`;
+    $graphF.style.height = $graphT.style.height = `${HEIGHT}px`;
+    const ctxF = $graphF.getContext('2d');
+    const ctxT = $graphT.getContext('2d');
+    (async () => {
+        let stream;
+        try {
+            stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        }
+        catch (error) {
+            alert(error);
+        }
+        if (stream) {
+            const audioContext = new AudioContext();
+            const analyser = audioContext.createAnalyser();
+            analyser.fftSize = 2048;
+            audioContext.createMediaStreamSource(stream).connect(analyser);
+            const update = ({ frequency, note, noteFrequency, deviation, octave, }) => {
+                $frequency.textContent = `${frequency.toFixed(2)} Hz`;
+                $nodeFrequency.textContent = `${noteFrequency.toFixed(2)} Hz`;
+                $note.textContent = note;
+                $octave.textContent = `${octave}`;
+                $deviation.textContent =
+                    deviation >= 1 ? '⬆' : deviation <= -1 ? '⬇' : '-';
+            };
+            const oscillator = audioContext.createOscillator();
+            oscillator.connect(audioContext.destination);
+            oscillator.start();
+            const loop = () => {
+                const f32buffer = new Float32Array(analyser.fftSize);
+                analyser.getFloatTimeDomainData(f32buffer);
+                let sum = 0.0;
+                for (const amplitude of f32buffer)
+                    sum += amplitude ** 2;
+                const volume = Math.sqrt(sum / f32buffer.length);
+                $volumeMeter.value = volume;
+                if (volume >= 0.05) {
+                    const frequency = autoCorrelate(f32buffer, audioContext.sampleRate);
+                    if (frequency) {
+                        const data = getDataFromFrequency(frequency);
+                        update(data);
+                        oscillator.frequency.value = data.noteFrequency;
+                    }
+                    else {
+                        oscillator.frequency.value = 0;
+                    }
+                }
+                else {
+                    oscillator.frequency.value = 0;
+                }
+                {
+                    const bytebuffer = new Uint8Array(analyser.fftSize);
+                    analyser.getByteTimeDomainData(bytebuffer);
+                    ctxF.clearRect(0, 0, WIDTH * devicePixelRatio, HEIGHT * devicePixelRatio);
+                    const rects = [];
+                    for (let i = 0; i < WIDTH * devicePixelRatio; i++) {
+                        const d = bytebuffer[~~((i / (WIDTH * devicePixelRatio)) * bytebuffer.length)];
+                        const dRate = d / 255;
+                        const [x, y] = [i, dRate * HEIGHT * devicePixelRatio];
+                        rects.push({ x, y });
+                    }
+                    ctxF.strokeStyle = 'white';
+                    ctxF.lineJoin = 'round';
+                    ctxF.lineWidth = 1 * devicePixelRatio;
+                    ctxF.beginPath();
+                    rects.forEach(({ x, y }, i) => {
+                        if (i)
+                            ctxF.lineTo(x, y);
+                        else
+                            ctxF.moveTo(x, y);
+                    });
+                    ctxF.stroke();
+                }
+                {
+                    const bytebuffer = new Uint8Array(analyser.fftSize);
+                    analyser.getByteFrequencyData(bytebuffer);
+                    ctxT.clearRect(0, 0, WIDTH * devicePixelRatio, HEIGHT * devicePixelRatio);
+                    const rects = [];
+                    for (let i = 0; i < WIDTH * devicePixelRatio; i++) {
+                        const d = bytebuffer[~~((i / (WIDTH * devicePixelRatio)) * bytebuffer.length)];
+                        const dRate = d / 255;
+                        const [x, y] = [
+                            i * devicePixelRatio,
+                            dRate * HEIGHT * devicePixelRatio,
+                        ];
+                        rects.push({ x, y });
+                    }
+                    ctxT.strokeStyle = 'white';
+                    ctxT.lineJoin = 'round';
+                    ctxT.lineWidth = 1 * devicePixelRatio;
+                    ctxT.beginPath();
+                    rects.forEach(({ x, y }, i) => {
+                        if (i)
+                            ctxT.lineTo(x, y);
+                        else
+                            ctxT.moveTo(x, y);
+                    });
+                    ctxT.stroke();
+                }
+                requestAnimationFrame(loop);
+            };
+            loop();
+        }
+    })();
+});
---
+++ build/pitcher.js
@@ -0,0 +1,62 @@
+const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+const A440 = 440;
+const MIDI = 69;
+const A = 2 ** (1 / 12);
+const C0 = 16.35;
+export const getDataFromFrequency = (frequency) => {
+    const N = Math.round(12 * Math.log2(frequency / A440));
+    const noteFrequency = A440 * A ** N;
+    const octave = Math.floor(Math.log2(noteFrequency / C0));
+    return {
+        frequency,
+        noteFrequency,
+        note: NOTES[(N + MIDI) % NOTES.length],
+        deviation: frequency - noteFrequency,
+        octave,
+    };
+};
+export const autoCorrelate = (buffer, sampleRate) => {
+    const RMS = Math.sqrt(buffer.reduce((acc, el) => acc + el ** 2, 0) / buffer.length);
+    if (RMS < 0.001)
+        return NaN;
+    const THRES = 0.2;
+    let r1 = 0;
+    let r2 = buffer.length - 1;
+    for (let i = 0; i < buffer.length / 2; ++i) {
+        if (Math.abs(buffer[i]) < THRES) {
+            r1 = i;
+            break;
+        }
+    }
+    for (let i = 1; i < buffer.length / 2; ++i) {
+        if (Math.abs(buffer[buffer.length - i]) < THRES) {
+            r2 = buffer.length - i;
+            break;
+        }
+    }
+    const buf2 = buffer.slice(r1, r2);
+    const c = new Array(buf2.length).fill(0);
+    for (let i = 0; i < buf2.length; ++i) {
+        for (let j = 0; j < buf2.length - i; ++j) {
+            c[i] = c[i] + buf2[j] * buf2[j + i];
+        }
+    }
+    let d = 0;
+    for (; c[d] > c[d + 1]; ++d)
+        ;
+    let maxval = -1;
+    let maxpos = -1;
+    for (let i = d; i < buf2.length; ++i) {
+        if (c[i] > maxval) {
+            maxval = c[i];
+            maxpos = i;
+        }
+    }
+    const T0 = maxpos;
+    const x1 = c[T0 - 1];
+    const x2 = c[T0];
+    const x3 = c[T0 + 1];
+    const a = (x1 + x3 - 2 * x2) / 2;
+    const b = (x3 - x1) / 2;
+    return sampleRate / (a ? T0 - b / (2 * a) : T0);
+};
