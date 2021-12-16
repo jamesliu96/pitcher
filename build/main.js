9fbc23ac704d975e7895222233c4e8c02f46d56b
James Liu <jamesliu96@users.noreply.github.com>
2021-12-16 16:25:03 +0800

    build

--- build/main.js
+++ build/main.js
@@ -1,4 +1,11 @@
 import { autoCorrelate, getDataFromFrequency, meanAmplitude, } from './pitcher.js';
+const drawPixel = ({ width, data }, x, y, r, g, b, a) => {
+    const idx = (x + y * width) * 4;
+    data[idx + 0] = r;
+    data[idx + 1] = g;
+    data[idx + 2] = b;
+    data[idx + 3] = a;
+};
 window.addEventListener('load', () => {
     const $volumeMeter = document.getElementById('volume_meter');
     const $frequency = document.getElementById('frequency');
@@ -8,32 +15,35 @@ window.addEventListener('load', () => {
     const $deviation = document.getElementById('deviation');
     const WIDTH = 800;
     const HEIGHT = 100;
+    const [w, h] = [WIDTH * devicePixelRatio, HEIGHT * devicePixelRatio];
     const $graphF = document.getElementById('graph_f');
     const $graphT = document.getElementById('graph_t');
+    const $graphP = document.getElementById('graph_p');
     const $graphOF = document.getElementById('graph_output_f');
     const $graphOT = document.getElementById('graph_output_t');
     $graphF.width =
         $graphT.width =
-            $graphOF.width =
-                $graphOT.width =
-                    WIDTH * devicePixelRatio;
-    $graphF.height =
-        $graphT.height =
-            $graphOF.height =
-                $graphOT.height =
-                    HEIGHT * devicePixelRatio;
+            $graphP.width =
+                $graphOF.width =
+                    $graphOT.width =
+                        w;
+    $graphF.height = $graphT.height = $graphOF.height = $graphOT.height = h;
+    $graphP.height = 4 * h;
     $graphF.style.width =
         $graphT.style.width =
-            $graphOF.style.width =
-                $graphOT.style.width =
-                    `${WIDTH}px`;
+            $graphP.style.width =
+                $graphOF.style.width =
+                    $graphOT.style.width =
+                        `${WIDTH}px`;
     $graphF.style.height =
         $graphT.style.height =
             $graphOF.style.height =
                 $graphOT.style.height =
                     `${HEIGHT}px`;
+    $graphP.style.height = `${4 * HEIGHT}px`;
     const ctxF = $graphF.getContext('2d');
     const ctxT = $graphT.getContext('2d');
+    const ctxP = $graphP.getContext('2d');
     const ctxOF = $graphOF.getContext('2d');
     const ctxOT = $graphOT.getContext('2d');
     (async () => {
@@ -79,12 +89,12 @@ window.addEventListener('load', () => {
                 {
                     const bytebuffer = new Uint8Array(inputAnalyser.fftSize);
                     inputAnalyser.getByteTimeDomainData(bytebuffer);
-                    ctxF.clearRect(0, 0, WIDTH * devicePixelRatio, HEIGHT * devicePixelRatio);
+                    ctxF.clearRect(0, 0, w, h);
                     const rects = [];
-                    for (let i = 0; i < WIDTH * devicePixelRatio; i++) {
-                        const d = bytebuffer[~~((i / (WIDTH * devicePixelRatio)) * bytebuffer.length)];
+                    for (let i = 0; i < w; i++) {
+                        const d = bytebuffer[~~((i / w) * bytebuffer.length)];
                         const dRate = d / 255;
-                        const [x, y] = [i, dRate * HEIGHT * devicePixelRatio];
+                        const [x, y] = [i, dRate * h];
                         rects.push({ x, y });
                     }
                     ctxF.strokeStyle = 'white';
@@ -102,13 +112,15 @@ window.addEventListener('load', () => {
                 {
                     const bytebuffer = new Uint8Array(inputAnalyser.frequencyBinCount);
                     inputAnalyser.getByteFrequencyData(bytebuffer);
-                    ctxT.clearRect(0, 0, WIDTH * devicePixelRatio, HEIGHT * devicePixelRatio);
+                    ctxT.clearRect(0, 0, w, h);
+                    const sli = ctxP.createImageData(w, 1);
                     const rects = [];
-                    for (let i = 0; i < WIDTH * devicePixelRatio; i++) {
-                        const d = bytebuffer[~~((i / (WIDTH * devicePixelRatio)) * bytebuffer.length)];
+                    for (let i = 0; i < w; i++) {
+                        const d = bytebuffer[~~((i / w) * bytebuffer.length)];
                         const dRate = d / 255;
-                        const [x, y] = [i, dRate * HEIGHT * devicePixelRatio];
+                        const [x, y] = [i, dRate * h];
                         rects.push({ x, y });
+                        drawPixel(sli, i, 0, 255, 255, 0, d);
                     }
                     ctxT.strokeStyle = 'white';
                     ctxT.lineJoin = 'round';
@@ -123,25 +135,25 @@ window.addEventListener('load', () => {
                     ctxT.stroke();
                     if (stable) {
                         ctxT.strokeStyle = 'yellow';
-                        ctxT.lineWidth = 2 * devicePixelRatio;
+                        ctxT.lineWidth = 3 * devicePixelRatio;
                         ctxT.beginPath();
-                        const x = (frequency / (audioContext.sampleRate / 2)) *
-                            WIDTH *
-                            devicePixelRatio;
+                        const x = (frequency / (audioContext.sampleRate / 2)) * w;
                         ctxT.moveTo(x, 0);
-                        ctxT.lineTo(x, HEIGHT * devicePixelRatio);
+                        ctxT.lineTo(x, h);
                         ctxT.stroke();
                     }
+                    ctxP.putImageData(ctxP.getImageData(0, 0, w, 4 * h), 0, 1);
+                    ctxP.putImageData(sli, 0, 0);
                 }
                 {
                     const bytebuffer = new Uint8Array(outputAnalyser.fftSize);
                     outputAnalyser.getByteTimeDomainData(bytebuffer);
-                    ctxOF.clearRect(0, 0, WIDTH * devicePixelRatio, HEIGHT * devicePixelRatio);
+                    ctxOF.clearRect(0, 0, w, h);
                     const rects = [];
-                    for (let i = 0; i < WIDTH * devicePixelRatio; i++) {
-                        const d = bytebuffer[~~((i / (WIDTH * devicePixelRatio)) * bytebuffer.length)];
+                    for (let i = 0; i < w; i++) {
+                        const d = bytebuffer[~~((i / w) * bytebuffer.length)];
                         const dRate = d / 255;
-                        const [x, y] = [i, dRate * HEIGHT * devicePixelRatio];
+                        const [x, y] = [i, dRate * h];
                         rects.push({ x, y });
                     }
                     ctxOF.strokeStyle = 'white';
@@ -159,12 +171,12 @@ window.addEventListener('load', () => {
                 {
                     const bytebuffer = new Uint8Array(outputAnalyser.frequencyBinCount);
                     outputAnalyser.getByteFrequencyData(bytebuffer);
-                    ctxOT.clearRect(0, 0, WIDTH * devicePixelRatio, HEIGHT * devicePixelRatio);
+                    ctxOT.clearRect(0, 0, w, h);
                     const rects = [];
-                    for (let i = 0; i < WIDTH * devicePixelRatio; i++) {
-                        const d = bytebuffer[~~((i / (WIDTH * devicePixelRatio)) * bytebuffer.length)];
+                    for (let i = 0; i < w; i++) {
+                        const d = bytebuffer[~~((i / w) * bytebuffer.length)];
                         const dRate = d / 255;
-                        const [x, y] = [i, dRate * HEIGHT * devicePixelRatio];
+                        const [x, y] = [i, dRate * h];
                         rects.push({ x, y });
                     }
                     ctxOT.strokeStyle = 'white';
@@ -180,13 +192,11 @@ window.addEventListener('load', () => {
                     ctxOT.stroke();
                     if (stable) {
                         ctxOT.strokeStyle = 'yellow';
-                        ctxOT.lineWidth = 2 * devicePixelRatio;
+                        ctxOT.lineWidth = 3 * devicePixelRatio;
                         ctxOT.beginPath();
-                        const x = (noteFrequency / (audioContext.sampleRate / 2)) *
-                            WIDTH *
-                            devicePixelRatio;
+                        const x = (noteFrequency / (audioContext.sampleRate / 2)) * w;
                         ctxOT.moveTo(x, 0);
-                        ctxOT.lineTo(x, HEIGHT * devicePixelRatio);
+                        ctxOT.lineTo(x, h);
                         ctxOT.stroke();
                     }
                 }
